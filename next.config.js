c7f566af79c6a5c053acb1c82416227abae8edf5
Bart Wijnants <bart.w@sparkcentral.com>
2020-05-01 20:14:38 +0200

    try to get the env vars

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    REGION: process.env.REGION,
+    USER_POOL_ID: process.env.USER_POOL_ID,
+    USER_POOL_WEB_CLIENT_ID: process.env.USER_POOL_WEB_CLIENT_ID,
+  },
+};
